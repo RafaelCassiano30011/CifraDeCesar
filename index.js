a3b850d9c8e3c94900cb8e5e2979f500018f67d5
rafael <rafael.souza@agenciaeplus.com.br>
2021-05-16 21:46:02 -0300

    Fiz o desafio

--- index.js
+++ index.js
@@ -4,19 +4,21 @@ const $span = document.querySelector('.span-cesar')
 const $button = document.querySelector('.button')
 
 
-const chacteres = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// const chacteres = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-$button.addEventListener('click', () => {
-    let frase = $input.value.toLowerCase()
-    let number = parseInt($inputNumber.value)
+// $button.addEventListener('click', () => {
+//     $span.textContent = ''
+//     let frase = $input.value.toLowerCase()
+//     let number = parseInt($inputNumber.value)
 
-    for (let i = 0; i < frase.length; i++) {
-        if (frase[i] != '') {
-            for (let x = 0; x < chacteres.length; x++) {
-                if (frase[i] == chacteres[x]) {
-                    a
-                }
-            }
-        }
-    }
-})
+//     for (let i = 0; i < frase.length; i++) {
+//         if (frase[i] != '') {
+//             for (let x = 0; x < chacteres.length; x++) {
+//                 if (frase[i] == chacteres[x]) {
+//                     cifra = chacteres[x + number]
+//                     $span.textContent += cifra
+//                 }
+//             }
+//         }
+//     }
+// })
